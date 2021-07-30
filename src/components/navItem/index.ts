4570323286d33972bff98535623a44e2ae3be3dc
style-it <styleit.api@gmail.com>
2021-07-30 09:58:51 +0300

    working on button

--- src/components/navItem/index.ts
+++ src/components/navItem/index.ts
@@ -1,26 +1,33 @@
 import { customElement, property } from 'lit/decorators.js';
-import { LitElement, html, css } from 'lit';
+import { LitElement, html, css, unsafeCSS } from 'lit';
+import btnCSS from '../../props/button';
+import theme from '../../theme';
 
 @customElement(`nav-item`)
 class NavbarItem extends LitElement {
   static get styles() {
+    const stylesProps = theme.navItem.props;
     return css`
     :host{
+            ${btnCSS};
             list-style-type: none;
             float: left !important;
             margin-right:.5rem;
             margin-left:.5rem;
             display: list-item;
             text-align: -webkit-match-parent;
+            font-family:${unsafeCSS(stylesProps.fontFamily)};
+      padding:${unsafeCSS(stylesProps.padding)};
+      line-height: ${unsafeCSS(stylesProps.lineHeight)};
+      font-size:${unsafeCSS(stylesProps.fontSize)};
   }`;
   }
   @property()
-
+  
   render() {
     return html`
-    <btn-bolder>
       <slot></slot>
-    </btn-bolder>      
+      <ripple-bolder/>
     `;
   }
 }
